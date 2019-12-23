2795e896941724c2bc68eae26ec66ac30137ec2d
Matt Grosso <mattgrosso@gmail.com>
2019-12-23 15:42:01 -0500

    Sets current user on select

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,12 @@
 const db = firebase.firestore();
 const memberList = [];
 let memberListReady = false;
+let currentUser;
 
 db.collection("members").get()
   .then((members) => {
     members.forEach((member) => {
-      memberList.push(member.data());
+      memberList.push({id: member.id, ...member.data()});
     })
 
     memberListReady = true;
@@ -17,6 +18,11 @@ db.collection("members").get()
   })
 
 const initialize = () => {
+  setUpNavEvents();
+  buildUserNameSelector();
+}
+
+function setUpNavEvents() {
   document.querySelectorAll(".nav-item").forEach((item) => {
     item.addEventListener("click", (event) => {
       const navValue = event.target.dataset.optionValue;
@@ -26,5 +32,28 @@ const initialize = () => {
       });
       document.querySelector(`section.${navValue}`).classList.add("active");
     })
+  })
+}
+
+function buildUserNameSelector() {
+  const usersNameSelector = document.querySelector(".users-name-selector");
+
+  memberList.forEach((member) => {
+    const memberName = member.name.nick || member.name.first;
+
+    const userNameOption = document.createElement("li");
+    userNameOption.classList.add("user-name-option");
+    userNameOption.setAttribute("data-user-name-option", member.id)
+
+    const userFullName = document.createElement("h2");
+    userFullName.innerText = memberName;
+    userNameOption.appendChild(userFullName);
+
+    userNameOption.addEventListener("click", (event) => {
+      currentUser = event.target.dataset.userNameOption
+      console.log("currentUser: ", currentUser);
+    });
+
+    usersNameSelector.appendChild(userNameOption);
   })
 }
